88453be4697c716d73e3dad8b5585f03070344d7
Ørjan Blom <blom@blom.tv>
2013-01-16 16:48:39 +0100

    Add more packages and add the array to the top

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -1,4 +1,32 @@
 class packages {
+  $packages = [
+    'autoconf',
+    'automake',
+    'bison',
+    'build-essential',
+    'curl',
+    'git',
+    'gnupg',
+    'libc6-dev',
+    'libreadline6',
+    'libreadline6-dev',
+    'libsqlite3-dev',
+    'libssl-dev',
+    'libtool',
+    'libxml2-dev',
+    'libyaml-dev',
+    'nodejs',
+    'npm',
+    'openjdk-6-jdk',
+    'openssl',
+    'pkg-config',
+    'sqlite3',
+    'subversion',
+    'vim',
+    'zlib1g',
+    'zlib1g-dev',
+  ]
+
   Exec { path => [ '/bin', '/sbin' , '/usr/bin', '/usr/sbin' ] }
 
   file { '/etc/apt/sources.list':
@@ -13,11 +41,6 @@ class packages {
     require => File['/etc/apt/sources.list'],
     unless  => 'ls /var/lib/apt/lists/mirrors.ubuntu.com_mirrors.txt_*',
   }
-
-  $packages = [
-    'git',
-    'vim',
-  ]
 
   package { $packages:
     ensure  => installed,
