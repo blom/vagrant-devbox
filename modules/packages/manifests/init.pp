5b8e980d71fa6caabd8c4f60b350bafd3914baea
Ørjan Blom <blom@blom.tv>
2013-01-16 16:57:42 +0100

    Install latest Puppet from apt.puppetlabs.com

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -46,4 +46,17 @@ class packages {
     ensure  => installed,
     require => Exec['apt resync'],
   }
+
+  apt::source { 'puppetlabs':
+    location   => 'http://apt.puppetlabs.com/',
+    repos      => 'main',
+    key        => '4BD6EC30',
+    key_server => 'pgp.mit.edu',
+    require    => Package[$packages],
+  }
+
+  package { 'puppet':
+    ensure  => latest,
+    require => Apt::Source['puppetlabs'],
+  }
 }
