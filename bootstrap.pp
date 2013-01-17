70101f75d364213cb8bcd1637496bb3e4f879805
Ørjan Blom <blom@blom.tv>
2013-01-17 20:08:33 +0100

    Setup MySQL

--- bootstrap.pp
+++ bootstrap.pp
@@ -1,1 +1,29 @@
 include packages
+include mysql::server::account_security
+
+class { 'mysql::server':
+  config_hash => {
+    'bind_address'  => '127.0.0.1',
+    'root_password' => 'vagrant',
+  },
+}
+
+Class['packages'] -> Class['mysql::server']
+
+database_user { 'vagrant@localhost':
+  password_hash => mysql_password('vagrant'),
+  require       => Class['mysql::server'],
+}
+
+database_grant { 'vagrant@localhost':
+  privileges => ['all'],
+  require    => Class['mysql::server'],
+}
+
+file { '/home/vagrant/.my.cnf':
+  ensure  => present,
+  owner   => 'vagrant',
+  group   => 'vagrant',
+  mode    => '0600',
+  content => "[client]\nuser=vagrant\npassword=vagrant\n"
+}
